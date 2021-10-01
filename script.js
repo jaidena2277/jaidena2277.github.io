fa7ce4ac3d2dd4038dadc2a59560870b66649c15
jaidena2277 <91745222+jaidena2277@users.noreply.github.com>
2021-10-01 11:16:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "jaiden ahamed"
+let userName = "jaidena2277.github.io";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
